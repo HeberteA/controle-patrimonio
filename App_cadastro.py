8f0c2b90283d402457ef348f2c01ff5595bd8f48
HeberteA <heberte.2307@gmail.com>
2025-09-19 09:10:51 -0300

    Update App_cadastro.py

--- App_cadastro.py
+++ App_cadastro.py
@@ -82,7 +82,7 @@ def tela_de_login():
         st.markdown(
             f"""
             <div style="display: flex; justify-content: center; margin-bottom: 20px;">
-                <img src="data:image/png;base64,{img_base64}" alt="Logo" width="1100">
+                <img src="data:image/png;base64,{img_base64}" alt="Logo" width="950">
             </div>
             """,
             unsafe_allow_html=True,
@@ -430,6 +430,7 @@ if not st.session_state.logged_in:
     tela_de_login()
 else:
     app_principal()
+
 
 
 
